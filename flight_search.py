98c6141e900fb6696f1bd7060e662fa7425963d5
jake <JAKEORTON99@GHMAIL.COM>
2025-10-31 14:21:39 +0000

    Update notes for development

--- flight_search.py
+++ flight_search.py
@@ -4,7 +4,19 @@ from datetime import datetime
 # Ordered or unordered linear search ? 
 #   benefits of ordered is that we can stop early during a search.
 # Binary search algorithm ? 
-# Ternary search algorithm ?
+#   if data cannot be sorted then you cannot use binary search.
+# Ternary search algorithm ? 
+#   Ternary search is a divide-and-conquer algorithm that works on sorted arrays.
+#  It divides the array into three parts and determines which part may contain the target value.
+
+# Create a data structure for each iteration of the search ? 
+#   this could be achieved by sorting data as it is collected from the data structure in the file manager.
+#   alternatively the data could be collected per key and then sorted into a list in the end through filtering and merging <- seems slower 
+
+# TODO : 
+#  connect to new flight manager.
+#  Create new search algorithm with clever sorting and search using tips above
+#  Make search more flexible
 
 # Search flights
 class FlightSearch:
